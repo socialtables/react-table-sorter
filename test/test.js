8daa71ebf36c9ec3f8f68a5756ae06d24f37c7a2
Ricky Vetter <ricky@socialtables.com>
2014-07-17 16:31:03 -0400

    added test descriptions

--- test/test.js
+++ test/test.js
@@ -1,24 +1,28 @@
-var assert = require("assert")
-describe('Connection', function(){
-  var db = new Connection
-    , tobi = new User('tobi')
-    , loki = new User('loki')
-    , jane = new User('jane');
 
-  beforeEach(function(done){
-    db.clear(function(err){
-      if (err) return done(err);
-      db.save([tobi, loki, jane], done);
-    });
-  })
 
-  describe('#find()', function(){
-    it('respond with matching records', function(done){
-      db.find({ type: 'User' }, function(err, res){
-        if (err) return done(err);
-        res.should.have.length(3);
-        done();
-      })
-    })
-  })
-})
+
+
+
+
+
+/*
+TESTS
+describe connection
+  it should connect to source if available
+  it should use data object if no source provided
+  it should use data object if source returns error
+
+describe render
+  it should render table if it finds data and config
+  it should render table with warning message if no data
+  it should render table of data without labels if no config
+  it should not render if no data or config
+  it should render links for entire rows where "link" attribute is present in config
+  it should replace "{cell}"" with cell's toString value when present in "link" attribute of config
+  it should render links for items with "link" attribute in data
+  it should render link from data over link from config
+
+describe interactions
+  it should sort items based on toString of item attributes
+  it should filter items based on toString index matching of item and filter text
+  it should filter numerical items based on operational values: ==, >, <, >=, <= */
