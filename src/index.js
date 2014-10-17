b750fc0bac5228aa36d113589ddbd780c8ebca7f
Ricky Vetter <ricky@socialtables.com>
2014-10-17 16:44:08 -0400

    remove text - just assumer text or component

--- src/index.js
+++ src/index.js
@@ -13,55 +13,12 @@ var TableSorter = module.exports = React.createClass({
   },
   componentWillReceiveProps: function(nextProps) {
     // Load new data when the dataSource property changes.
-    if (nextProps.dataSource != this.props.dataSource) {
-      this.loadData(nextProps.dataSource);
+    if (nextProps.data != this.props.data) {
+      this.setState({items: nextProps.data});
     }
   },
   componentWillMount: function() {
-    this.loadData(this.props.dataSource, this.props.data);
-  },
-  loadData: function(dataSource, data) {
-    if (!data){
-      request = new XMLHttpRequest();
-      request.open('GET', dataSource, true);
-
-      request.onload = function() {
-        if (request.status >= 200 && request.status < 400){
-          // Success!
-          data = JSON.parse(request.responseText);
-          console.log(data);
-          data.forEach(function(item){
-          for (var key in item) {
-              if(typeof item[key] !== 'object') {
-                item[key] = {"text":item[key]}
-              }
-            }
-          });
-          console.log("Received data");
-          this.setState({items: data});
-        } else {
-          // We reached our target server, but it returned an error
-          console.log("Error loading JSON");
-        }
-      }.bind(this);
-
-      request.onerror = function() {
-        // There was a connection error of some sort
-        console.log("Connection error. Could not retrieve JSON");
-      };
-
-      request.send();
-    } else{
-      console.log(data);
-      data.forEach(function(item){
-      for (var key in item) {
-          if(typeof item[key] !== 'object') {
-            item[key] = {"text":item[key]}
-          }
-        }
-      });
-      this.setState({items: data});
-   }
+    this.setState({items: this.props.data});
   },
   handleFilterTextChange: function(column) {
     return function(newValue) {
@@ -142,16 +99,8 @@ var TableSorter = module.exports = React.createClass({
 
     var cell = function(x) {
       return columnNames.map(function(c) {
-        //custom link defined in data
-        if (x[c].link){
-          return <td><a href={x[c].link}>{x[c].text}</a></td>;
-        // link defined in config for entire column
-        } else if (this.props.config.columns[c].link) {
-         var link = this.props.config.columns[c].link.replace("{cell}", x[c].text);
-          return <td><a href={link}>{x[c]}</a></td>;
-        // simple cell
-        } else {
-          return <td>{x[c].text}</td>;
+        	// simple cell
+          return <td>{x[c]}</td>;
         }
       }, this);
     }.bind(this);
