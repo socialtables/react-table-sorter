c2736c4152052441f51c7fd1a97da9e8ec7bf2d4
Ricky Vetter <ricky@socialtables.com>
2014-10-17 16:32:04 -0400

    remove react dependency

--- src/index.js
+++ src/index.js
@@ -1,216 +1,172 @@
 /** @jsx React.DOM */
-var React = require("react");
 var _ = require("lodash");
 
 // TableSorter React Component
 var TableSorter = module.exports = React.createClass({
-  getInitialState: function() {
-    return {
-      items: this.props.initialItems || [],
-      sort: this.props.config.sort || { column: "", order: "" },
-      columns: this.props.config.columns
-    };
-  },
-  componentWillReceiveProps: function(nextProps) {
-    // Load new data when the dataSource property changes.
-    if (nextProps.dataSource != this.props.dataSource || nextProps.data != this.props.data) {
-      this.loadData(nextProps.dataSource, nextProps.data);
-    }
-  },
-  componentWillMount: function() {
-    this.loadData(this.props.dataSource, this.props.data);
-  },
-  loadData: function(dataSource, data) {
-    if (!data){
-      request = new XMLHttpRequest();
-      request.open('GET', dataSource, true);
+	getInitialState: function() {
+		return {
+			items: this.props.initialItems || [],
+			sort: this.props.config.sort || { column: "", order: "" },
+			columns: this.props.config.columns
+		};
+	},
+	componentWillReceiveProps: function(nextProps) {
+		// Load new data when the data changes.
+		if (nextProps.data != this.props.data) {
+			this.loadData(nextProps.data);
+		}
+	},
+	componentWillMount: function() {
+		this.loadData(this.props.data);
+	},
+	loadData: function(data) {
+		data.forEach(function(item){
+			for (var key in item) {
+				if(typeof item[key] !== 'object' || item[key].props) {
+					item[key] = {"text":item[key]}
+				}
+			}
+		});
+		this.setState({items: data});
+	},
+	handleFilterTextChange: function(column) {
+		return function(newValue) {
+			var obj = this.state.columns;
+			obj[column].filterText = newValue;
 
-      request.onload = function() {
-        if (request.status >= 200 && request.status < 400){
-          // Success!
-          data = JSON.parse(request.responseText);
-          console.log(data);
-          data.forEach(function(item){
-          for (var key in item) {
-              // allows text or react components
-              if(typeof item[key] !== 'object' || item[key].props) {
-                item[key] = {"text":item[key]}
-              }
-            }
-          });
-          console.log("Received data");
-          this.setState({items: data});
-        } else {
-          // We reached our target server, but it returned an error
-          console.log("Error loading JSON");
-        }
-      }.bind(this);
+			// Since we have already mutated the state, just call forceUpdate().
+			// Ideally we'd copy and setState or use an immutable data structure.
+			this.forceUpdate();
+		}.bind(this);
+	},
+	columnNames: function() {
+		return Object.keys(this.state.columns); 
+	},
+	sortColumn: function(column) {
+		return function(event) {
+			var newSortOrder = (this.state.sort.order == "asc") ? "desc" : "asc";
 
-      request.onerror = function() {
-        // There was a connection error of some sort
-        console.log("Connection error. Could not retrieve JSON");
-      };
+			if (this.state.sort.column != column) {
+				newSortOrder = this.state.columns[column].defaultSortOrder;
+			}
 
-      request.send();
-    } else{
-      console.log(data);
-      data.forEach(function(item){
-      for (var key in item) {
-          if(typeof item[key] !== 'object' || item[key].props) {
-            item[key] = {"text":item[key]}
-          }
-        }
-      });
-      this.setState({items: data});
-   }
-  },
-  handleFilterTextChange: function(column) {
-    return function(newValue) {
-      var obj = this.state.columns;
-      obj[column].filterText = newValue;
+			this.setState({sort: { column: column, order: newSortOrder }});
+		}.bind(this);
+	},
+	sortClass: function(column) {
+		var ascOrDesc = (this.state.sort.order == "asc") ? "headerSortAsc" : "headerSortDesc";
+		return (this.state.sort.column == column) ? ascOrDesc : "";
+	},
+	render: function() {
+		var rows = [];
 
-      // Since we have already mutated the state, just call forceUpdate().
-      // Ideally we'd copy and setState or use an immutable data structure.
-      this.forceUpdate();
-    }.bind(this);
-  },
-  columnNames: function() {
-     return Object.keys(this.state.columns); 
-  },
-  sortColumn: function(column) {
-    return function(event) {
-      var newSortOrder = (this.state.sort.order == "asc") ? "desc" : "asc";
+		var columnNames = this.columnNames();
+		var filters = {};
 
-      if (this.state.sort.column != column) {
-          newSortOrder = this.state.columns[column].defaultSortOrder;
-      }
+		// Inequality function map for the filtering
+		var operators = {
+		"<": function(x, y) { return x < y; },
+		"<=": function(x, y) { return x <= y; },
+		">": function(x, y) { return x > y; },
+		">=": function(x, y) { return x >= y; },
+		"==": function(x, y) { return x == y; }
+		};
 
-      this.setState({sort: { column: column, order: newSortOrder }});
-    }.bind(this);
-  },
-  sortClass: function(column) {
-    var ascOrDesc = (this.state.sort.order == "asc") ? "headerSortAsc" : "headerSortDesc";
-    return (this.state.sort.column == column) ? ascOrDesc : "";
-  },
-  render: function() {
-    var rows = [];
+		var operandRegex = /^((?:(?:[<>]=?)|==))\s?([-]?\d+(?:\.\d+)?)$/;
 
-    var columnNames = this.columnNames();
-    var filters = {};
+		columnNames.forEach(function(column) {
+			var filterText = this.state.columns[column].filterText;
+			filters[column] = null;
 
-      // Inequality function map for the filtering
-    var operators = {
-      "<": function(x, y) { return x < y; },
-      "<=": function(x, y) { return x <= y; },
-      ">": function(x, y) { return x > y; },
-      ">=": function(x, y) { return x >= y; },
-      "==": function(x, y) { return x == y; }
-    };
+			if (filterText.length > 0) { 
+				operandMatch = operandRegex.exec(filterText);
 
-    var operandRegex = /^((?:(?:[<>]=?)|==))\s?([-]?\d+(?:\.\d+)?)$/;
+				if (operandMatch && operandMatch.length == 3) {
+					filters[column] = function(match) { return function(x) { return operators[match[1]](x, match[2]); }; }(operandMatch); 
+				}
+				else {
+					filters[column] = function(x) {
+						return (x.toString().toLowerCase().indexOf(filterText.toLowerCase()) > -1);
+					};
+				}
+			}
+		}, this);
 
-    columnNames.forEach(function(column) {
-      var filterText = this.state.columns[column].filterText;
-      filters[column] = null;
+		var filteredItems = _.filter(this.state.items, function(item) {
+			return _.every(columnNames, function(c) {
+				return (!filters[c] || filters[c](item[c].text));
+			}, this);
+		}, this);
 
-      if (filterText.length > 0) { 
-        operandMatch = operandRegex.exec(filterText);
-        if (operandMatch && operandMatch.length == 3) {
-          filters[column] = function(match) { return function(x) { return operators[match[1]](x, match[2]); }; }(operandMatch); 
-        } else {
-          filters[column] = function(x) {
-            return (x.toString().toLowerCase().indexOf(filterText.toLowerCase()) > -1);
-          };
-        }
-      }
-    }, this);
-    
-    var filteredItems = _.filter(this.state.items, function(item) {
-      return _.every(columnNames, function(c) {
-        return (!filters[c] || filters[c](item[c].text));
-      }, this);
-    }, this);
+		var sortedItems = _.sortBy(filteredItems, this.state.sort.column);
 
-    var sortedItems = _.sortBy(filteredItems, this.state.sort.column);
+		if (this.state.sort.order === "desc") {
+			sortedItems.reverse();
+		}
 
-    if (this.state.sort.order === "desc") sortedItems.reverse();
+		var headerExtra = function() {
+			return columnNames.map(function(c) {
+				return <th className="header-extra">{this.state.columns[c].name}</th>;
+			}, this);   
+		}.bind(this);
 
-    var headerExtra = function() {
-      return columnNames.map(function(c) {
-        return <th className="header-extra">{this.state.columns[c].name}</th>;
-      }, this);   
-    }.bind(this);
+		var cell = function(x) {
+			return columnNames.map(function(c) {
+				return <td>{x[c].text}</td>;
+			}, this);
+		}.bind(this);
 
-    var cell = function(x) {
-      return columnNames.map(function(c) {
-        //custom link defined in data
-        if (x[c].link){
-          return <td><a href={x[c].link}>{x[c].text}</a></td>;
-        // link defined in config for entire column
-        } else if (this.props.config.columns[c].link) {
-         var link = this.props.config.columns[c].link.replace("{cell}", x[c].text);
-          return <td><a href={link}>{x[c]}</a></td>;
-        // simple cell
-        } else {
-          return <td>{x[c].text}</td>;
-        }
-      }, this);
-    }.bind(this);
+		var keyIndex = 0;
+		sortedItems.forEach(function(item, idx) {
+			var headerRepeat = parseInt(this.props.headerRepeat, 10);
+			if ((this.props.headerRepeat > 0) && (idx > 0) && (idx % this.props.headerRepeat === 0)) {
+				rows.push (
+					<tr key={"header-"+keyIndex}>
+					  { headerExtra() }
+					</tr>
+				)
+			}
+			rows.push(
+				<tr key={keyIndex}>
+					{ cell(item) }
+				</tr>
+			);
+			keyIndex++;
+		}.bind(this));
 
-    var keyIndex = 0;
-    sortedItems.forEach(function(item, idx) {
-      var headerRepeat = parseInt(this.props.headerRepeat, 10);
-      if ((this.props.headerRepeat > 0) && 
-          (idx > 0) &&
-          (idx % this.props.headerRepeat === 0)) {
+		var filterLink = function(column) {
+			return {
+				value: this.state.columns[column].filterText,
+				requestChange: this.handleFilterTextChange(column)
+			};
+		}.bind(this);
 
-          rows.push (
-            <tr key={"header-"+keyIndex}>
-              { headerExtra() }
-            </tr>
-          )
-      }
+		var header = columnNames.map(function(c) {
+			return <th onClick={this.sortColumn(c)} className={"header " + this.sortClass(c)}>{this.state.columns[c].name}</th>;
+		}, this);
 
-      rows.push(
-        <tr key={keyIndex}>
-          { cell(item) }
-        </tr>
-      );
-      keyIndex++;
-    }.bind(this));
+		var filterInputs = columnNames.map(function(c) {
+			return (
+				<td>
+					<input type="text" valueLink={filterLink(c)} />
+				</td>
+			);
+		}, this);
 
-    var filterLink = function(column) {
-      return {
-        value: this.state.columns[column].filterText,
-        requestChange: this.handleFilterTextChange(column)
-      };
-    }.bind(this);
-
-    var header = columnNames.map(function(c) {
-      return <th onClick={this.sortColumn(c)} className={"header " + this.sortClass(c)}>{this.state.columns[c].name}</th>;
-    }, this);
-
-    var filterInputs = columnNames.map(function(c) {
-      return <td><input type="text" valueLink={filterLink(c)} /></td>;
-    }, this);
-
-    return (
-      <table cellSpacing="0" className="tablesorter">
-        <thead>
-          <tr>
-            { header }
-          </tr>
-          <tr>
-            { filterInputs }
-          </tr>
-        </thead>
-        <tbody>
-          { rows }
-        </tbody>
-      </table>
-    );
-  }
+		return (
+			<table cellSpacing="0" className="tablesorter">
+				<thead>
+					<tr>
+						{ header }
+					</tr>
+					<tr>
+						{ filterInputs }
+					</tr>
+				</thead>
+				<tbody>
+					{ rows }
+				</tbody>
+			</table>
+		);
+	}
 });
-
-
-
