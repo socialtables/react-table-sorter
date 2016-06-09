b70ebfc2a5e2ac7e737ad00e6d862023bcfcbaf0
yalcindo <yalcindo1@gmail.com>
2016-06-09 16:43:55 -0400

    add react 0.13.3

--- src/index.js
+++ src/index.js
@@ -79,7 +79,7 @@ var TableSorter = module.exports = React.createClass({
     }.bind(this);
   },
   columnNames: function() {
-     return Object.keys(this.state.columns); 
+     return Object.keys(this.state.columns);
   },
   sortColumn: function(column) {
     return function(event) {
@@ -117,10 +117,10 @@ var TableSorter = module.exports = React.createClass({
       var filterText = this.state.columns[column].filterText;
       filters[column] = null;
 
-      if (filterText.length > 0) { 
+      if (filterText.length > 0) {
         operandMatch = operandRegex.exec(filterText);
         if (operandMatch && operandMatch.length == 3) {
-          filters[column] = function(match) { return function(x) { return operators[match[1]](x, match[2]); }; }(operandMatch); 
+          filters[column] = function(match) { return function(x) { return operators[match[1]](x, match[2]); }; }(operandMatch);
         } else {
           filters[column] = function(x) {
             return (x.toString().toLowerCase().indexOf(filterText.toLowerCase()) > -1);
@@ -128,7 +128,7 @@ var TableSorter = module.exports = React.createClass({
         }
       }
     }, this);
-    
+
     var filteredItems = _.filter(this.state.items, function(item) {
       return _.every(columnNames, function(c) {
         return (!filters[c] || filters[c](item[c].text));
@@ -141,22 +141,22 @@ var TableSorter = module.exports = React.createClass({
 
     var headerExtra = function() {
       return columnNames.map(function(c) {
-        return <th className="header-extra">{this.state.columns[c].name}</th>;
-      }, this);   
+        return <th key={this.state.columns[c].name} className="header-extra">{this.state.columns[c].name}</th>;
+      }, this);
     }.bind(this);
 
     var cell = function(x) {
       return columnNames.map(function(c) {
         //custom link defined in data
         if (x[c].link){
-          return <td><a href={x[c].link}>{x[c].text}</a></td>;
+          return <td key={x[c].link} ><a href={x[c].link}>{x[c].text}</a></td>;
         // link defined in config for entire column
         } else if (this.props.config.columns[c].link) {
          var link = this.props.config.columns[c].link.replace("{cell}", x[c].text);
-          return <td><a href={link}>{x[c]}</a></td>;
+          return <td key={link}><a href={link}>{x[c]}</a></td>;
         // simple cell
         } else {
-          return <td>{x[c].text}</td>;
+          return <td key={c+"-"+x[c]}>{x[c].text}</td>;
         }
       }, this);
     }.bind(this);
@@ -164,10 +164,9 @@ var TableSorter = module.exports = React.createClass({
     var keyIndex = 0;
     sortedItems.forEach(function(item, idx) {
       var headerRepeat = parseInt(this.props.headerRepeat, 10);
-      if ((this.props.headerRepeat > 0) && 
+      if ((this.props.headerRepeat > 0) &&
           (idx > 0) &&
           (idx % this.props.headerRepeat === 0)) {
-
           rows.push (
             <tr key={"header-"+keyIndex}>
               { headerExtra() }
@@ -176,7 +175,7 @@ var TableSorter = module.exports = React.createClass({
       }
 
       rows.push(
-        <tr key={keyIndex}>
+        <tr key={"row-"+item.id.text}>
           { cell(item) }
         </tr>
       );
@@ -191,20 +190,21 @@ var TableSorter = module.exports = React.createClass({
     }.bind(this);
 
     var header = columnNames.map(function(c) {
-      return <th onClick={this.sortColumn(c)} className={"header " + this.sortClass(c)}>{this.state.columns[c].name}</th>;
+      return <th  key={"header-" + this.state.columns[c].name} onClick={this.sortColumn(c)} className={"header " + this.sortClass(c)}>{this.state.columns[c].name}</th>;
     }, this);
 
     var filterInputs = columnNames.map(function(c) {
-      return <td><input type="text" valueLink={filterLink(c)} /></td>;
+
+      return <td key={c}><input type="text" valueLink={filterLink(c)} /></td>;
     }, this);
 
     return (
       <table cellSpacing="0" className="tablesorter">
         <thead>
-          <tr>
+          <tr key="header-first-row">
             { header }
           </tr>
-          <tr>
+          <tr key="header-second-row">
             { filterInputs }
           </tr>
         </thead>
@@ -215,6 +215,3 @@ var TableSorter = module.exports = React.createClass({
     );
   }
 });
-
-
-
